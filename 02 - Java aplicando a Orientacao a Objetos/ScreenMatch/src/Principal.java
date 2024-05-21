5a3ac056d4b004d16f786da626d04e2a905ea4a5
Alleph Nogueira <allephn@hotmail.com.br>
2024-05-20 22:13:38 -0300

    Private - public - get - set - pacotes - import

--- 02 - Java aplicando a Orientacao a Objetos/ScreenMatch/src/Principal.java
+++ 02 - Java aplicando a Orientacao a Objetos/ScreenMatch/src/Principal.java
@@ -1,23 +1,25 @@
+import br.com.alleph.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.nome = "O Senhor dos Aneis";
         meuFilme.anoDeLancamento = 2000;
         meuFilme.duracaoEmMinutos = 180;
-        meuFilme.totalDeAvaliacoes = 0;
+        meuFilme.getTotalDeAvaliacoes();
 
 
         meuFilme.exibiFichaTecnica();
+
 
         meuFilme.avalia(8);
         meuFilme.avalia(10);
         meuFilme.avalia(6);
         meuFilme.avalia(4);
-        System.out.println("Total de avaliadores: " + meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliadores: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Media dos avaliadores: " + meuFilme.calculaMedia());
 
         System.out.println("Esse filme tem " + meuFilme.idadeFilme() + " anos");
-
 
         System.out.println("_____________________");
         System.out.println("_____________________");
@@ -28,14 +30,14 @@ public class Principal {
         filme2.nome = "Harry Potter";
         filme2.anoDeLancamento = 2016;
         filme2.duracaoEmMinutos = 110;
-        filme2.totalDeAvaliacoes = 0;
+        filme2.getTotalDeAvaliacoes();
 
         filme2.exibiFichaTecnica();
         filme2.avalia(8);
         filme2.avalia(4);
         filme2.avalia(10);
 
-        System.out.println("2 Total de avaliadores: " + filme2.totalDeAvaliacoes);
+        System.out.println("2 Total de avaliadores: " + filme2.getTotalDeAvaliacoes());
         System.out.println("2 Media dos avaliadores: " + filme2.calculaMedia());
 
         System.out.println("2 Esse filme tem " + filme2.idadeFilme() + " anos");
--- 02 - Java aplicando a Orientacao a Objetos/ScreenMatch/src/br/com/alleph/screenmatch/modelos/Filme.java
+++ 02 - Java aplicando a Orientacao a Objetos/ScreenMatch/src/br/com/alleph/screenmatch/modelos/Filme.java
@@ -1,30 +1,32 @@
+package br.com.alleph.screenmatch.modelos;
+
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incuidoNoPlano;
+    public String nome;
+    public int anoDeLancamento;
+    public boolean incuidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes; // Privado só pode ser acessado dentro da propria classe / Ou invocar ele dentro de um metodo para usar fora.
-    int duracaoEmMinutos;
+    public int duracaoEmMinutos;
 
-    int getTotalDeAvaliacoes(){
+    public int getTotalDeAvaliacoes(){
         return totalDeAvaliacoes;
     }
 
-    void exibiFichaTecnica() {   // Esse e meu metodo.
+    public void exibiFichaTecnica() {   // Esse e meu metodo.
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lancamento: " + anoDeLancamento);
     }
 
-    void avalia(double nota) {
+    public void avalia(double nota) {
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
     }
 
-    double calculaMedia() {
+    public double calculaMedia() {
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 
-    int idadeFilme(){
+    public int idadeFilme(){
         return 2024 - anoDeLancamento;
     }
 }
